c94616d344a3f0dd71706fa0f6c675eb8050427f
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-09-30 00:06:55 -0300

    Algumas configurações

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App'
 import store from './store'
+import router from './router'
+
+import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 
-new Vue({
+window._Vue = new Vue({
   router,
   store,
   render: h => h(App)
